438218ced32f571c767545158819589fb4dca9ad
Franck Fernandez <franck.fernandez@gmail.com>
2022-05-31 09:17:16 +0200

    Done https://www.youtube.com/watch\?v\=ylFrG4m7FjU

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Sse } from '@nestjs/common';
+import { interval, map, Observable } from 'rxjs';
 import { AppService } from './app.service';
 
-@Controller()
+interface MessageEvent {
+  data: string | object;
+}
+
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Sse('event')
+  sendEvent(): Observable<MessageEvent> {
+    return interval(1000).pipe(
+      map((num: number) => ({
+        data: `Hello ${num}`,
+      })),
+    );
   }
 }
